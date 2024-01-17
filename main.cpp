e5f931eb1ef56898a4f8c82c949edc22a16221e1
valkyrie_pilot <valk@randomairborne.dev>
2024-01-17 16:12:57 -0700

    improve testing

--- main.cpp
+++ main.cpp
@@ -13,8 +13,14 @@ int main() {
         list.Set(i, 0);
     }
     for (int i = 0; i <= 50; ++i) {
-        auto val = *list.Get(i);
-        std::cout << std::to_string(val) << std::endl;
+        const int val = *list.Get(i);
+        int target;
+        if (i <= 25) target = 0;
+        else target = i * 2;
+        std::cout << "val: " << std::to_string(val) << " target: "<< std::to_string(target) << std::endl;
+        if (target != val) {
+            std::exit(1);
+        }
     }
     return 0;
 }
